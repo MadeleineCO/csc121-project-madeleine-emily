b46416ecd9a80c9b67155440854f18e68f0b4610
Madeleine Odom <madeleineodom@gmail.com>
2023-09-14 15:44:52 -0400

    comment

--- csc121-creative-project/src/SnakeApp.java
+++ csc121-creative-project/src/SnakeApp.java
@@ -1,4 +1,8 @@
 
+/**
+ * Represents an interactive application where a snake eats an apple 
+ * to grow longer. The user must use the arrow keys to control the snake.
+ */
 class SnakeApp 
 {
 	jhcbjhwebjwebdwbedfbw
