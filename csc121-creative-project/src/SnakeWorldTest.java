4d36ab591008a27d0fc9552764d2ee3ceffdb32a
Madeleine Odom <madeleineodom@gmail.com>
2023-10-02 17:39:45 -0400

    no los

--- csc121-creative-project/src/SnakeWorldTest.java
+++ csc121-creative-project/src/SnakeWorldTest.java
@@ -11,11 +11,6 @@ class SnakeWorldTest {
     Apple a = new Apple(new Posn(300, 300));
     Apple a2 = new Apple(new Posn(50, 50));
 
-    LoS los1 = new LoS(sl1, null);
-    LoS los1v2 = new LoS(sl1, new LoS(sl2, null)); 
-    LoS los1v3 = new LoS(sl2, new LoS(sl1, null));
-    LoS los2 = new LoS(sl1, new LoS(sl2, new LoS(sl3, null)));
-
     ILoP mt = new MTLoP();
     ILoP lst = new ConsLoP(new Posn(100, 120), new ConsLoP(new Posn(120, 120), mt));
     ILoP lstApp = new ConsLoP(new Posn(100, 120), new ConsLoP(new Posn(120, 120), 
@@ -41,36 +36,6 @@ class SnakeWorldTest {
         assertFalse(new Snake(new PosnList().append(new Posn(570, 300)), SnakeWorld.RIGHT, 57).hitWall());
         assertTrue(new Snake(new PosnList().append(new Posn(570, 300)), SnakeWorld.RIGHT, 0).hitWall());
     }
-
-    /*
-    @Test
-    void testHitBySnake() {
-        assertFalse(a.hitBySnake(new Posn(0,0)));
-        assertTrue(a2.hitBySnake(new Posn(60,60)));
-        assertTrue(a2.hitBySnake(new Posn(51, 51)));
-        assertFalse(a2.hitBySnake(new Posn(51, 81)));
-        assertFalse(a2.hitBySnake(new Posn(79, 79)));
-        assertTrue(a2.hitBySnake(new Posn(60, 40)));
-        assertFalse(a2.hitBySnake(new Posn(60, 90)));
-    }
-
-    @Test 
-    void testAddBoxInList() {
-        assertEquals(los1, new LoS(sl1, null));
-        assertEquals(los1v2, new LoS(sl1, new LoS(sl2, null)));
-
-        los1 = los1.addBoxInList(los1, sl2);
-        assertEquals(los1, new LoS(sl1, new LoS(sl2, null))); 
-
-        assertFalse(los1.equals(new LoS(sl2, new LoS(sl1, null))));		
-        assertFalse(los1.equals(new LoS(sl1, null)));  
-
-        los2 = los2.addBoxInList(los2, sl1);
-        assertEquals(los2, new LoS(sl1, new LoS(sl2, new LoS(sl3, new LoS(sl1, null)))));
-
-    } 
-    
-    */
 
 
     @Test
